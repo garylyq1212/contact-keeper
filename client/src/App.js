d3f934fa63f809579571a3bceedb8a5d41e4c95f
GaryQuan <garylyq17@gmail.com>
2019-08-27 22:59:56 +0800

    Added PrivateRoute Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Alerts from './components/layout/Alerts';
 
+import PrivateRoute from './components/routing/PrivateRoute';
+
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
@@ -33,7 +35,7 @@ const App = () => {
               <div className='container'>
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
+                  <PrivateRoute exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/register' component={Register} />
                   <Route exact path='/login' component={Login} />
