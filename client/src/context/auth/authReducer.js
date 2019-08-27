28ed4648d366c90b0c7b9556902ae2e95076ec90
GaryQuan <garylyq17@gmail.com>
2019-08-27 22:52:59 +0800

    Handle user login action & logout action

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -20,6 +20,7 @@ export default (state, action) => {
       };
 
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -29,6 +30,8 @@ export default (state, action) => {
       };
 
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
+    case LOGOUT:
     case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
